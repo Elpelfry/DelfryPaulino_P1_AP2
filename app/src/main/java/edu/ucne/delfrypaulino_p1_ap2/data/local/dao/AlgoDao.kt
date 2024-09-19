1dd0a2d161be5b2b97d6c651313b12811a493f5e
Elpelfry <delfry_paulino@ucne.edu.do>
2024-09-18 21:05:30 -0400

    Agrega AlgoEntity, Parcialdb y Dao

---
+++ app/src/main/java/edu/ucne/delfrypaulino_p1_ap2/data/local/dao/AlgoDao.kt
@@ -0,0 +1,30 @@
+package edu.ucne.delfrypaulino_p1_ap2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.delfrypaulino_p1_ap2.data.local.entities.AlgoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlgoDao {
+    @Upsert
+    suspend fun save(algo: AlgoEntity)
+
+    @Query(
+        """
+        SELECT * 
+        FROM Algos
+        WHERE id =:id
+        LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): AlgoEntity?
+
+    @Delete
+    suspend fun delete(algo: AlgoEntity)
+
+    @Query("SELECT * FROM Algos")
+    fun getall(): Flow<List<AlgoEntity>>
+}
