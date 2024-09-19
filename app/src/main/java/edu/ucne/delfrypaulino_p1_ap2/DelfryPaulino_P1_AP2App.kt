4744b63c4412d0425c35948a9bc61a39d6e268a0
Elpelfry <delfry_paulino@ucne.edu.do>
2024-09-18 20:06:39 -0400

    Agrega hilt

---
+++ app/src/main/java/edu/ucne/delfrypaulino_p1_ap2/DelfryPaulino_P1_AP2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.delfrypaulino_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DelfryPaulino_P1_AP2App : Application() {
+}
