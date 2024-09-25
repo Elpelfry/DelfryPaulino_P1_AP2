8378960670acefd29e35f8c11ff98889e585bb06
Elpelfry <delfry_paulino@ucne.edu.do>
2024-09-25 18:52:24 -0400

    Uistate

---
+++ app/src/main/java/edu/ucne/delfrypaulino_p1_ap2/presentation/venta/Uistate.kt
@@ -0,0 +1,21 @@
+package edu.ucne.delfrypaulino_p1_ap2.presentation.venta
+
+import edu.ucne.delfrypaulino_p1_ap2.data.local.entities.VentaEntity
+
+data class Uistate(
+    val cliente: String = "",
+    val galones: Double? = 0.0,
+    val descuentoGalon: Double? = 0.0,
+    val precio: Double? = 0.0,
+    val totalDescontado: Double? = 0.0,
+    val total: Double? = 0.0,
+    var validation: Boolean = false,
+    val ventaId: Int = 0,
+    val ventas: List<VentaEntity> = emptyList(),
+    val clienteError: String = "",
+    val galonesError: String = "",
+    val descuentoGalonError: String = "",
+    val precioError: String = "",
+    val totalDescontadoError: String = "",
+    val totalError: String = "",
+)
