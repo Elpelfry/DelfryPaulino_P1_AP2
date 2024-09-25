5553106de76aa3a8a1aba7c1dfe455d5861f2a31
Elpelfry <delfry_paulino@ucne.edu.do>
2024-09-25 18:51:21 -0400

    remover db de algo y agregar db de venta

--- app/src/main/java/edu/ucne/delfrypaulino_p1_ap2/di/AppModule.kt
+++ app/src/main/java/edu/ucne/delfrypaulino_p1_ap2/di/AppModule.kt
@@ -24,5 +24,5 @@ object AppModule {
             .build()
 
     @Provides
-    fun provideAlgoDao(parcial1Db: Parcial1Db) = parcial1Db.algoDao()
+    fun provideMetaDao(parcial1Db: Parcial1Db) = parcial1Db.ventaDao()
 }
