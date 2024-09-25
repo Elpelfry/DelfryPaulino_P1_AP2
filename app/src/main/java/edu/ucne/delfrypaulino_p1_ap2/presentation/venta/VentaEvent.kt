be3150298490d9b5685407d56f8ce90d79023f4b
Elpelfry <delfry_paulino@ucne.edu.do>
2024-09-25 18:52:13 -0400

    VentaEvent

---
+++ app/src/main/java/edu/ucne/delfrypaulino_p1_ap2/presentation/venta/VentaEvent.kt
@@ -0,0 +1,15 @@
+package edu.ucne.delfrypaulino_p1_ap2.presentation.venta
+
+sealed interface VentaEvent {
+    data object save : VentaEvent
+    data class delete (val ventaId: Int): VentaEvent
+    data object validation : VentaEvent
+    data object newVenta : VentaEvent
+    data class onClienteChanged(val cliente : String) : VentaEvent
+    data class onGalonesChanged(val galones : String) : VentaEvent
+    data class onDescuentoGalonChanged(val descuentoGalon : String) : VentaEvent
+    data class onPrecioChanged(val precio : String) : VentaEvent
+    data class onTotalDescontadoChanged(val totalDescontado : String) : VentaEvent
+    data class onTotalChanged(val total : String) : VentaEvent
+    data class selectVenta(val ventaId: Int) : VentaEvent
+}
